dc74a2662c92e05ca0851735c71d46faef8d8438
Paul Pessoa <paulmspessoa@gmail.com>
2023-03-15 00:31:02 -0300

    iniciado projeto calculadora de estagio

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as React from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
